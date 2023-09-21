474919cc839f51d2f1fe89e9d300ea5eca27c315
OMKIRAN REDDY <99016756+kiranreddy627@users.noreply.github.com>
2023-09-21 17:33:14 +0530

    Time: 85 ms (84.01%), Space: 16.5 MB (69.30%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -3,9 +3,9 @@ class Solution:
         lst1=nums1+nums2
         lst1.sort()
         n=len(lst1)
-        summ=0
+        s=0
         if n%2==0:
-            summ=(lst1[n//2]+lst1[n//2-1])/2
+            s=(lst1[n//2]+lst1[n//2-1])/2
         else:
-            summ=lst1[(n)//2]
-        return summ
+            s=lst1[(n)//2]
+        return s
